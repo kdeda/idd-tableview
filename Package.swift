91f956ae7dc115ef8ecee9e992235026a576d0ec
Klajd Deda <kdeda@id-design.com>
2022-02-18 00:53:53 -0500

    Fixes

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["TableView"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kdeda/idd-log4-swift.git", from: "1.1.1")
+        .package(url: "https://github.com/kdeda/idd-log4-swift.git", from: "1.1.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
