4316b625a8482cd15bef60d52e5d44744ce6943d
Klajd Deda <kdeda@id-design.com>
2023-04-08 12:13:00 -0400

    Pinning dependencies

--- Package.swift
+++ Package.swift
@@ -9,17 +9,14 @@ let package = Package(
         .macOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TableView",
             targets: ["TableView"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kdeda/idd-log4-swift.git", from: "1.2.1")
+        .package(url: "https://github.com/kdeda/idd-log4-swift.git", "1.2.5" ..< "2.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TableView",
             dependencies: [
