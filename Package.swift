ab68152adafb76ed1b93a3c4832f2ef9e36abd57
Klajd Deda <kdeda@id-design.com>
2023-04-08 13:59:37 -0400

    Pinning dependencies. to log4swift 2.x

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["TableView"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kdeda/idd-log4-swift.git", "1.2.5" ..< "2.0.0"),
+        .package(url: "https://github.com/kdeda/idd-log4-swift.git", "2.0.1" ..< "3.0.0"),
     ],
     targets: [
         .target(
